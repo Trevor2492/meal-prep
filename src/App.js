574ff0c559dedd9e6eded99b03a97f7472dc8e64
Trevor Allen <trevor2492@gmail.com>
2021-07-18 20:37:27 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import mealList from "./api";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [meal, setMeal] = useState("");
+
+  useEffect(() => {}, []);
+
+  const handleClick = () => {
+    alert("you clicked");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Tonight we're having: {mealList[1]}</div>
+      <button onClick={() => handleClick()}>Click me</button>
     </div>
   );
 }

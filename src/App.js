9bff5ac7db6038373e7f96da1fa6793a0943afde
Trevor Allen <trevor2492@gmail.com>
2021-07-21 21:01:13 -0500

    added button styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
 import mealList from "./api";
 import { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import CachedIcon from "@material-ui/icons/Cached";
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
 
 function App() {
   let [meal, setMeal] = useState(0);
+  const classes = useStyles();
 
   const handleClick = () => {
     // sets num to be a random integer that represents an index number from mealList
@@ -15,7 +25,15 @@ function App() {
   return (
     <div className="App">
       <div>Tonight we're having: {mealList[meal]}</div>
-      <button onClick={() => handleClick()}>Click me</button>
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.button}
+        startIcon={<CachedIcon />}
+        onClick={() => handleClick()}
+      >
+        Randomize
+      </Button>
     </div>
   );
 }
